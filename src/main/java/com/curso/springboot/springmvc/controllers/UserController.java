3f9db498e310d0e22bc1a9e1da00e9e7606e82a1
ctobonpe <ctobonpe@gmail.com>
2025-04-28 14:29:31 -0500

    Controlador,Modelo, RestController y pasar datos a vista

--- src/main/java/com/curso/springboot/springmvc/controllers/UserController.java
+++ src/main/java/com/curso/springboot/springmvc/controllers/UserController.java
@@ -4,15 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.curso.springboot.springmvc.models.User;
+
 @Controller
 public class UserController {
 	
 	@GetMapping("/details")
 	public String details(Model model) {
 		
+		User user = new User("Adrian", "Tobon");
+		
 		model.addAttribute("title","Spring MVC");
-		model.addAttribute("name","Adrian");
-		model.addAttribute("last_name","Tobon");		
+		model.addAttribute("user",user);
+			
 		return "details";
 	}
 
--- src/main/java/com/curso/springboot/springmvc/controllers/UserRestController.java
+++ src/main/java/com/curso/springboot/springmvc/controllers/UserRestController.java
@@ -7,16 +7,20 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.curso.springboot.springmvc.models.User;
+
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
 	
 	@GetMapping("/details")
 	public Map<String, Object> details() {
+		
+		User user = new User("Adrian", "Tobon");
+		
 		Map<String, Object> body = new HashMap<>();
 		body.put("title","Spring MVC");
-		body.put("name","Adrian");
-		body.put("last_name","Tobon");
+		body.put("user",user);		
 		
 		return body;
 	}
