32847275d65ba511e0ca931fd6b15e9003e39823
ctobonpe <ctobonpe@gmail.com>
2025-04-28 15:56:23 -0500

    request mapping

--- src/main/java/com/curso/springboot/springmvc/controllers/UserRestController.java
+++ src/main/java/com/curso/springboot/springmvc/controllers/UserRestController.java
@@ -3,17 +3,21 @@ package com.curso.springboot.springmvc.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.curso.springboot.springmvc.models.User;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class UserRestController {
 	
 	@GetMapping("/details")
+	@ResponseBody
 	public Map<String, Object> details() {
 		
 		User user = new User("Adrian", "Tobon");
@@ -24,5 +28,20 @@ public class UserRestController {
 		
 		return body;
 	}
+	
+	
+	@RequestMapping(path="/details_2", method= RequestMethod.GET)
+	@ResponseBody
+	public Map<String, Object> details_2() {
+		
+		User user = new User("Adrian", "Tobon");
+		
+		Map<String, Object> body = new HashMap<>();
+		body.put("title","Spring MVC");
+		body.put("user",user);		
+		
+		return body;
+	}
+	
 
 }
