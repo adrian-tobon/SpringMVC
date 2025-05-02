a589f49e5c0e4d580475472704195f6fb563287f
ctobonpe <ctobonpe@gmail.com>
2025-05-02 11:22:07 -0500

    map con lenguaje de expresion en Value, objeto environment, redirect y forward

--- src/main/java/com/curso/springboot/springmvc/controllers/PathVariableController.java
+++ src/main/java/com/curso/springboot/springmvc/controllers/PathVariableController.java
@@ -4,7 +4,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,21 +24,35 @@ public class PathVariableController {
 
 	@Value("${config.username}")
 	private String username;
-	
-//	  @Value("${config.message}")
-//	  private String message;
+
+	@Value("${config.message}")
+	private String message;
 
 	@Value("${config.code}")
 	private Integer code;
 
 	@Value("${config.listOfValues}")
 	private List<String> listOfValues;
-	
+
 	@Value("#{'${config.listOfValues}'.split(',')}")
 	private List<String> valueList;
-	
+
 	@Value("#{'${config.listOfValues}'.toUpperCase()}")
 	private String list;
+
+	@Value("#{${config.valuesMap}}")
+	private Map<String, Object> valuesMap;
+
+	@Value("#{${config.valuesMap}.product}")
+	private String product;
+
+	@Value("#{${config.valuesMap}.price}")
+	private int price;
+	 
+	
+	@Autowired
+	private Environment environment;
+
 
 	@GetMapping("/baz/{message}")
 	public ParamDTO baz(@PathVariable String message) {
@@ -70,11 +86,32 @@ public class PathVariableController {
 		Map<String, Object> json = new HashMap<>();
 		json.put("code", code);
 		json.put("username", username);
-		json.put("message", message);
+		json.put("message", message);		
 		json.put("list_of_values", listOfValues);
 		json.put("value_list", valueList);
 		json.put("list", list);
+		json.put("valuesMap", valuesMap);
+		json.put("product", product);
+		json.put("price", price);
 		return json;
 	}
+	
+	@GetMapping("/environment")
+	public Map<String, Object> environment() {
+
+		Map<String, Object> json = new HashMap<>();
+		json.put("code", environment.getProperty("config.code",Integer.class));
+		json.put("username", environment.getProperty("config.username"));
+		json.put("message", environment.getProperty("config.message"));
+		
+		json.put("list_of_values",  environment.getProperty("config.listOfValues"));		
+		json.put("valuesMap", environment.getProperty("config.valuesMap"));
+		
+		//json.put("product", environment.getProperty("config.code"));
+		//json.put("price", environment.getProperty("config.code"));
+		return json;
+	}
+	
+	
 
 }
