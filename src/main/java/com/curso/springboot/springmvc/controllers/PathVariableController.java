6233ffab8ad09070288489cc9a8666d189c1f321
ctobonpe <ctobonpe@gmail.com>
2025-05-02 07:29:12 -0500

    inyeccion valores properties y cambio codificacion a UTF-8

--- src/main/java/com/curso/springboot/springmvc/controllers/PathVariableController.java
+++ src/main/java/com/curso/springboot/springmvc/controllers/PathVariableController.java
@@ -1,5 +1,9 @@
 package com.curso.springboot.springmvc.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,35 +19,53 @@ import com.curso.springboot.springmvc.models.dto.ParamsDTO;
 @RequestMapping("/api/var")
 public class PathVariableController {
 
+	@Value("${config.username}")
+	private String username;
+	
+//	  @Value("${config.message}")
+//	  private String message;
+
+	@Value("${config.code}")
+	private Integer code;
+
+	@Value("${config.listOfValues}")
+	private String[] listOfValues;
+
 	@GetMapping("/baz/{message}")
 	public ParamDTO baz(@PathVariable String message) {
-		
-	ParamDTO param = new ParamDTO();
+
+		ParamDTO param = new ParamDTO();
 		param.setMessage(message);
 		return param;
 	}
-	
+
 	@GetMapping("/bazs/{text}/{code}")
 	public ParamsDTO bazs(@PathVariable String text, @PathVariable Integer code) {
-		
+
 		Integer codigo = 0;
 		ParamsDTO params = new ParamsDTO();
 		params.setText(text);
-		
-//		try {
-//			codigo = code;			
-//		}catch(MethodArgumentTypeMismatchException e) {}
-		
 		params.setCode(codigo);
 		return params;
 	}
-	
+
 	@PostMapping("/create")
 	public User create(@RequestBody User user) {
-		
-		//en teoria, crear un usario en la DB
-		user.setName(user.getName().toUpperCase());		
+
+		// en teoria, crear un usario en la DB
+		user.setName(user.getName().toUpperCase());
 		return user;
 	}
-	
+
+	@GetMapping("/values")
+	public Map<String, Object> values(@Value("${config.message}") String message) {
+
+		Map<String, Object> json = new HashMap<>();
+		json.put("code", code);
+		json.put("username", username);
+		json.put("message", message);
+		json.put("list_of_values", listOfValues);
+		return json;
+	}
+
 }
